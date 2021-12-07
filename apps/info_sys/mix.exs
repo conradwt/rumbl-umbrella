3810f77d9fd557c815509b04823ba50bdadf59a4
Conrad Taylor <conradwt@gmail.com>
2021-12-06 22:34:35 -0800

    Upgrade to Elixir 1.13.0 (#2)

--- apps/info_sys/mix.exs
+++ apps/info_sys/mix.exs
@@ -9,7 +9,7 @@ defmodule InfoSys.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.11",
+      elixir: "~> 1.13.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
