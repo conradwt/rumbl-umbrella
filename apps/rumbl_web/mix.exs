8183b6d5aafaeaeda7ffe86520d5336589617121
Conrad Taylor <conradwt@gmail.com>
2022-05-31 22:55:56 -0700

    Upgrade to Elixir 1.13.4.

--- apps/rumbl_web/mix.exs
+++ apps/rumbl_web/mix.exs
@@ -9,7 +9,7 @@ defmodule RumblWeb.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.13.3",
+      elixir: "~> 1.13.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
